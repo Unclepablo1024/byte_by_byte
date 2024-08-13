2c55326f1d072f9e7b627e34bd4f17b224234fb4
SypherFleur <jasoninwaneti@gmail.com>
2024-08-12 21:30:10 -0500

    Renamed Bossmana and Fixed Boss Update

--- code/boss_mana.py
+++ code/boss_mana.py
@@ -1,4 +1,5 @@
 import pygame
+import config
 from boss1 import Boss
 
 
@@ -12,8 +13,41 @@ class GameManager:
 
     def defeat_enemy(self):
         self.enemies_defeated += 1
-        if self.enemies_defeated >= 5 and self.Boss:
+        if self.enemies_defeated >= config.MAX_ENEMIES and self.Boss:
             self.Boss.set_walking(True)  # Start boss walking out
     def update(self):
          if self.Boss:
              self.Boss.update()
+
+
+class Boss_HealthBar:
+    def __init__(self, max_health=config.BHEALTH_BAR_MAX_HEALTH, width=config.BHEALTH_BAR_WIDTH,
+                 height=config.BHEALTH_BAR_HEIGHT,
+                 x=config.BHEALTH_BAR_X, y=config.BHEALTH_BAR_Y, color=config.BHEALTH_BAR_COLOR):
+        self.max_health = max_health
+        self.current_health = max_health
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def update_health(self, amount):
+        self.current_health = max(0, min(self.max_health, self.current_health + amount))
+        print(f"Updated health: {self.current_health}")
+
+    
+
+    def is_depleted(self):
+        return self.current_health <= 0
+
+    def draw(self, screen):
+        # Calculate the proportion of health left
+        health_ratio = self.current_health / self.max_health
+        # Update the color based on health ratio (green to red)
+        self.color = (255 * (1 - health_ratio), 255 * health_ratio, 0)
+        # Draw the background bar
+        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, self.width, self.height))
+        # Draw the current health bar
+        current_width = self.width * health_ratio
+        pygame.draw.rect(screen, self.color, (self.x, self.y, current_width, self.height))
